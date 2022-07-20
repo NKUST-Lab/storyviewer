9810b48af12a1dc038125ba2904cd5b671e0add6
Agry88 <rgok307085@gmail.com>
2022-07-20 22:39:10 +0800

    Fix:make canvas Responsive progress more smooth

--- src/Hooks/useResize.js
+++ src/Hooks/useResize.js
@@ -1,11 +1,24 @@
 import React, { useState, useEffect } from 'react';
 
 function useResize() {
+
     const [firstLoad, setfirstLoad] = useState(true);
-    const [windowWidth, setwindowWidth] = useState(window.innerWidth);
-    window.addEventListener("resize", () => setwindowWidth(window.innerWidth));
-    //避免Line16提早和Line10重複執行
-    window.addEventListener("resize", () => setfirstLoad(false));
+
+    function debounce(func) {
+        let timer;
+        return () => {
+            if (timer) clearTimeout(timer);
+            timer = setTimeout(func, 1000);
+        };
+    }
+
+    useEffect(() => {
+        window.addEventListener("resize", debounce(() => {
+            setfirstLoad(false);
+            setCanvasSize();
+            console.log("Resized");
+        }));
+    }, []);
 
     //剛Load進執行
     useEffect(() => {
@@ -13,20 +26,10 @@ function useResize() {
     }, [])
 
 
-    useEffect(() => {
+    const setCanvasSize = () => {
         //避免首次重複執行
         if (firstLoad === true) return
 
-        const setCanvastimer = setTimeout(() => {
-            setCanvasSize();
-        }, 2000);
-
-        return () => {
-            clearTimeout(setCanvastimer);
-        }
-    }, [windowWidth]);
-
-    const setCanvasSize = () => {
         const canvas = document.getElementById("preview");
         const ctx = canvas.getContext("2d"); //取得Dom元素
         let scaleX
@@ -37,7 +40,7 @@ function useResize() {
         ctx.resetTransform();
 
         //取得使用者的視窗大小減掉 342 使最大尺寸為1578
-        const resizedCanvasWidth = windowWidth - 342;
+        const resizedCanvasWidth = window.innerWidth - 342;
 
         //透過比例算出畫布適合高度
         const ratio = resizedCanvasWidth / 2224;
@@ -65,7 +68,6 @@ function useResize() {
 
         nextButton.style.top = `${buttonHeight}px`;
         nextButton.style.left = `${canvasWidth - (100 * scaleX) - buttonSize}px`;
-        return
     }
 
 
--- src/Pages/Storyviewer.js
+++ src/Pages/Storyviewer.js
@@ -1,8 +1,7 @@
 import { useParams } from 'react-router-dom';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import { drawBackgroundImage, drawText, drawAccessories, drawCharacter } from './../functions/draw';
-import useResize from './../Hooks/useResize';
-
+import useResize from '../Hooks/useResize';
 
 function Storyviewer() {
 
@@ -25,10 +24,11 @@ function Storyviewer() {
             })
     }, [])
 
+    //繪畫Function
     const draw = async () => {
         const currentContent = allcontent[page];
         console.log("doDraw");
-        console.log(currentContent);
+        console.log("currentContent", currentContent);
 
         const canvas = document.getElementById("preview");
         const ctx = canvas.getContext("2d"); //取得Dom元素
@@ -51,33 +51,76 @@ function Storyviewer() {
 
 
     //處理RWD
-    useResize();
-    //當視窗大小更動時觸發重新繪畫
-    const [windowWidth, setwindowWidth] = useState(0);
-    window.addEventListener("resize", () => setwindowWidth(window.innerWidth));
-    useEffect(() => {
-        //Prevent Load in First time
-        if (windowWidth === 0) return
-        const drawtimer = setTimeout(() => {
-            draw();
-        }, 2000);
-
+    const [windowWidth, setwindowWidth] = useState(window.innerWidth);
+    const debounce = (func) => {
+        let timer;
         return () => {
-            clearTimeout(drawtimer);
-        }
-    }, [windowWidth]);
+            if (timer) clearTimeout(timer);
+            timer = setTimeout(func, 1000);
+        };
+    }
 
+    useEffect(() => {
+        resize();
+        window.addEventListener("resize", debounce(() => {
+            resize();
+            setwindowWidth(window.innerWidth);
+        }));
+    }, []);
+
+    //
+    const resize = useCallback(() => {
+            const canvas = document.getElementById("preview");
+            const ctx = canvas.getContext("2d"); //取得Dom元素
+            let scaleX
+            let scaleY
+            console.log("setCanvasSize");
+
+            //原尺寸2224 * 1668
+            ctx.resetTransform();
+
+            //取得使用者的視窗大小減掉 342 使最大尺寸為1578
+            const resizedCanvasWidth = window.innerWidth - 342;
+
+            //透過比例算出畫布適合高度
+            const ratio = resizedCanvasWidth / 2224;
+            const resizedCanvasHeight = 1668 * ratio > 1080 ? 1080 : 1668 * ratio;
+
+            //設定畫布大小及縮放
+            ctx.canvas.width = resizedCanvasWidth;
+            ctx.canvas.height = resizedCanvasHeight;
+            scaleX = resizedCanvasWidth / 2224
+            scaleY = resizedCanvasHeight / 1668;
+            ctx.scale(scaleX, scaleY);
+
+
+            //設定按鈕大小
+            const prevButton = document.getElementsByClassName("btn-prev")[0];
+            const nextButton = document.getElementsByClassName("btn-next")[0];
+
+            //設定按鈕位置
+            const buttonHeight = resizedCanvasHeight - 200;
+            prevButton.style.top = `${buttonHeight}px`;
+            prevButton.style.left = `${100 * scaleX}px`;
+
+            const canvasWidth = document.getElementById("preview").width;
+            const buttonSize = document.getElementsByClassName("btn-page")[0].clientWidth;
+
+            nextButton.style.top = `${buttonHeight}px`;
+            nextButton.style.left = `${canvasWidth - (100 * scaleX) - buttonSize}px`;
+        },[windowWidth])
+        
 
     //換頁時重新繪畫
     useEffect(() => {
 
-        //開始繪畫
         draw();
 
         return () => {
-            setisButtonDisabled(true); //未載入畫面前不可前往下一頁
+            //未載入畫面前不可前往下一頁
+            setisButtonDisabled(true);
         }
-    }, [page, allcontent]);
+    }, [page, allcontent , resize]);
 
     return (
         <div className='container'>
