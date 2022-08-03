73b625b0440f6e635e280f112c45ef71a8335ccb
Agry88 <rgok307085@gmail.com>
2022-08-03 22:01:07 +0800

    Fix:Fix page switch bug and make canvas bigger and keep the ratio

--- src/Pages/Storyviewer.js
+++ src/Pages/Storyviewer.js
@@ -70,54 +70,60 @@ function Storyviewer() {
     }, []);
 
     const resize = useCallback(() => {
-            const canvas = document.getElementById("preview");
-            const ctx = canvas.getContext("2d"); //取得Dom元素
-            let scaleX
-            let scaleY
-            console.log("setCanvasSize");
+        const canvas = document.getElementById("preview");
+        const ctx = canvas.getContext("2d"); //取得Dom元素
+        let scaleX
+        let scaleY
+        console.log("setCanvasSize");
 
-            //原尺寸2224 * 1668
-            ctx.resetTransform();
+        //原尺寸2224 * 1668
+        ctx.resetTransform();
 
-            //取得使用者的視窗大小使Canvas等於視窗大小的0.64
-            const resizedCanvasWidth = window.innerWidth * 0.64
-            
-            //透過比例算出畫布適合高度
-            const ratio = resizedCanvasWidth / 2224;
-            const resizedCanvasHeight = 1668 * ratio
+        //取得使用者的視窗大小使Canvas等於視窗大小的0.9
+        let resizedCanvasWidth = window.innerWidth * 0.9
 
+        //透過比例算出畫布適合高度
+        let ratio = resizedCanvasWidth / 2224;
+        let resizedCanvasHeight = 1668 * ratio
 
-            //設定畫布大小及縮放
-            ctx.canvas.width = resizedCanvasWidth;
-            ctx.canvas.height = resizedCanvasHeight;
-            scaleX = resizedCanvasWidth / 2224
-            scaleY = resizedCanvasHeight / 1668;
-            ctx.scale(scaleX, scaleY);
+        if (resizedCanvasHeight > window.innerHeight) {
+            resizedCanvasHeight = window.innerHeight
+            ratio = resizedCanvasHeight / 1668
+            resizedCanvasWidth = 2224 * ratio
+        }
 
 
-            //設定按鈕大小
-            const prevButton = document.getElementsByClassName("btn-prev")[0];
-            const nextButton = document.getElementsByClassName("btn-next")[0];
-            //Every 25px from 1920 to userWindow reduce the size of Buttons 1px
-            const responsivecondition = (1920 - window.innerWidth)/25
-            const buttonSize = 150
-            const resizedButtonSize = buttonSize - responsivecondition
-            prevButton.style.width = `${resizedButtonSize}px`;
-            prevButton.style.height = `${resizedButtonSize}px`;
-            nextButton.style.width = `${resizedButtonSize}px`;
-            nextButton.style.height = `${resizedButtonSize}px`;
-            
-            //設定按鈕位置
-            const buttonTop = resizedCanvasHeight - resizedButtonSize;
-            prevButton.style.top = `${buttonTop}px`;
-            prevButton.style.left = `${100 * scaleX}px`;
+        //設定畫布大小及縮放
+        ctx.canvas.width = resizedCanvasWidth;
+        ctx.canvas.height = resizedCanvasHeight;
+        scaleX = resizedCanvasWidth / 2224
+        scaleY = resizedCanvasHeight / 1668;
+        ctx.scale(scaleX, scaleY);
 
-            const canvasWidth = document.getElementById("preview").width;
 
-            nextButton.style.top = `${buttonTop}px`;
-            nextButton.style.left = `${canvasWidth - (100 * scaleX) - resizedButtonSize}px`;
-        },[windowWidth])
-        
+        //設定按鈕大小
+        const prevButton = document.getElementsByClassName("btn-prev")[0];
+        const nextButton = document.getElementsByClassName("btn-next")[0];
+        //Every 25px from 1920 to userWindow reduce the size of Buttons 1px
+        const responsivecondition = (1920 - window.innerWidth) / 25
+        const buttonSize = 150
+        const resizedButtonSize = buttonSize - responsivecondition
+        prevButton.style.width = `${resizedButtonSize}px`;
+        prevButton.style.height = `${resizedButtonSize}px`;
+        nextButton.style.width = `${resizedButtonSize}px`;
+        nextButton.style.height = `${resizedButtonSize}px`;
+
+        //設定按鈕位置
+        const buttonTop = resizedCanvasHeight - resizedButtonSize;
+        prevButton.style.top = `${buttonTop}px`;
+        prevButton.style.left = `${100 * scaleX}px`;
+
+        const canvasWidth = document.getElementById("preview").width;
+
+        nextButton.style.top = `${buttonTop}px`;
+        nextButton.style.left = `${canvasWidth - (100 * scaleX) - resizedButtonSize}px`;
+    }, [windowWidth])
+
 
     //換頁時重新繪畫
     useEffect(() => {
@@ -128,24 +134,26 @@ function Storyviewer() {
             //未載入畫面前不可前往下一頁
             setis_button_disabled(true);
         }
-    }, [page_number, book_page_content , resize]);
+    }, [page_number, book_page_content, resize]);
 
     const handleSetPage = (_page_number) => {
+
+        setpage_number(_page_number)
+        console.log("page",page_number)
 
         document.querySelectorAll("button.btn-prev")[0].style.display = "block"
         document.querySelectorAll("button.btn-next")[0].style.display = "block"
 
-        if(_page_number === 0){
+        if (_page_number === 0) {
             document.querySelectorAll("button.btn-prev")[0].style.display = "none"
             return
         }
 
-        if(_page_number === total_page_number){
+        if (_page_number === total_page_number - 1) {
             document.querySelectorAll("button.btn-next")[0].style.display = "none"
             return
         }
 
-        setpage_number(_page_number)
     }
 
 
